72034135eb652bad5f7acc299a2c560255f8ebab
Benjamin Grier <b.grier@aims.gov.au>
2024-04-15 13:37:28 +1000

    Add Indigenous Land Use Agreements script

---
+++ src/9_add_Indigenous_Land_Use_Agreements.jl
@@ -0,0 +1,38 @@
+using
+    CSV,
+    Dates,
+    DataFrames
+
+using
+    GLMakie,
+    GeoMakie
+
+using
+    Statistics,
+    Bootstrap
+
+using GeoIO
+import GeoDataFrames as GDF
+import GeoFormatTypes as GFT
+import ArchGDAL as AG
+
+include("common.jl")
+
+#reformat ILUA shapefile to geojson for loading with GeoDataFrames
+ILUA_geotable_init = GeoIO.load(joinpath(DATA_DIR, "indigenous_land_use_agreements/ILUA_Registered_Notified_Nat.shp"))
+GeoIO.save("Indigenous_Land_Use_Agreements.geojson", ILUA_geotable_init)
+
+#load input data
+RRAP_lookup = GDF.read(joinpath(OUTPUT_DIR, "rrap_shared_lookup.gpkg"))
+ILUA_zones = GDF.read(joinpath(DATA_DIR, "Indigenous_Land_Use_Agreements.geojson"))
+
+#find intersections and join to RRAP_lookup
+RRAP_ILUA_zones = find_intersections(RRAP_lookup, ILUA_zones, :GBRMPA_ID, :NAME, :geometry)
+RRAP_lookup = leftjoin(RRAP_lookup, RRAP_ILUA_zones, on=:GBRMPA_ID, matchmissing=:notequal, order=:left)
+
+#format data for output
+rename!(RRAP_lookup, Dict(:area_ID => :Indigenous_Land_Use_Agreement))
+RRAP_lookup.Indigenous_Land_Use_Agreement .= ifelse.(ismissing.(RRAP_lookup.Indigenous_Land_Use_Agreement), "NA", RRAP_lookup.Indigenous_Land_Use_Agreement)
+RRAP_lookup.Indigenous_Land_Use_Agreement = convert.(String, RRAP_lookup.Indigenous_Land_Use_Agreement)
+
+GDF.write(joinpath(OUTPUT_DIR, "rrap_shared_lookup.gpkg"), RRAP_lookup; crs=GFT.EPSG(4326))
