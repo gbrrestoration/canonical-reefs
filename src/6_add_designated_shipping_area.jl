1ba1193ec2033376ab946cb3a14ba65cacfaa5b0
Benjamin Grier <b.grier@aims.gov.au>
2024-04-15 12:03:58 +1000

    Add shipping and cruise transit

---
+++ src/6_add_designated_shipping_area.jl
@@ -0,0 +1,35 @@
+using
+    CSV,
+    Dates,
+    DataFrames
+
+using
+    GLMakie,
+    GeoMakie
+
+using
+    Statistics,
+    Bootstrap
+
+import GeoDataFrames as GDF
+import GeoFormatTypes as GFT
+import ArchGDAL as AG
+
+include("common.jl")
+
+
+#load input data
+RRAP_lookup = GDF.read(joinpath(OUTPUT_DIR, "rrap_shared_lookup.gpkg"))
+shipping_areas = GDF.read(joinpath(DATA_DIR, "Great_Barrier_Reef_Marine_Park_Designated_Shipping_Areas_201_2053803872900928951.gpkg"))
+shipping_areas[1,:NAME] = "Other GBR Areas" #renames the GBR area that is not shipping-exclusion to other gbr areas
+
+#find intersections and join to RRAP_lookup
+RRAP_shipping = find_intersections(RRAP_lookup, shipping_areas, :GBRMPA_ID, :NAME, :SHAPE, Proportion=true)
+RRAP_lookup = leftjoin(RRAP_lookup, RRAP_shipping, on = :GBRMPA_ID, matchmissing = :notequal, order = :left)
+
+#format data for output
+rename!(RRAP_lookup, Dict(:area_ID => :designated_shipping_area))
+RRAP_lookup.designated_shipping_area .= ifelse.(ismissing.(RRAP_lookup.designated_shipping_area), "NA", RRAP_lookup.designated_shipping_area)
+RRAP_lookup.designated_shipping_area = convert.(String, RRAP_lookup.designated_shipping_area)
+
+GDF.write(joinpath(OUTPUT_DIR, "rrap_shared_lookup.gpkg"), RRAP_lookup; crs=GFT.EPSG(4326))
---
+++ src/7_add_cruise_transit_lanes.jl
@@ -0,0 +1,38 @@
+using
+    CSV,
+    Dates,
+    DataFrames
+
+using
+    GLMakie,
+    GeoMakie
+
+using
+    Statistics,
+    Bootstrap
+
+import GeoDataFrames as GDF
+import GeoFormatTypes as GFT
+import ArchGDAL as AG
+
+include("common.jl")
+
+
+#load input data
+RRAP_lookup = GDF.read(joinpath(OUTPUT_DIR, "rrap_shared_lookup.gpkg"))
+cruise_transit = GDF.read(joinpath(DATA_DIR, "Great_Barrier_Reef_Marine_Park_Cruise_Ship_Transit_Lanes_20_-6667226202001620759.gpkg"))
+
+#find intersections and join to RRAP_lookup
+RRAP_cruise_transit = find_intersections(RRAP_lookup, cruise_transit, :GBRMPA_ID, :AREA_DESCR, :SHAPE, Proportion=true)
+rename!(RRAP_cruise_transit, Dict(:area_ID => :AREA_DESCR))
+RRAP_cruise_transit = leftjoin(RRAP_cruise_transit, cruise_transit[:,[:AREA_DESCR,:NOTES]], on=:AREA_DESCR, matchmissing=:notequal, order=:left)
+RRAP_lookup = leftjoin(RRAP_lookup, RRAP_cruise_transit, on=:GBRMPA_ID, matchmissing=:notequal, order=:left)
+
+#format data for output
+rename!(RRAP_lookup, Dict(:AREA_DESCR => :cruise_transit_lane, :NOTES => :cruise_transit_notes))
+RRAP_lookup.cruise_transit_lane .= ifelse.(ismissing.(RRAP_lookup.cruise_transit_lane), "NA", RRAP_lookup.cruise_transit_lane)
+RRAP_lookup.cruise_transit_lane = convert.(String, RRAP_lookup.cruise_transit_lane)
+RRAP_lookup.cruise_transit_notes .= ifelse.(ismissing.(RRAP_lookup.cruise_transit_notes), "NA", RRAP_lookup.cruise_transit_notes)
+RRAP_lookup.cruise_transit_notes = convert.(String, RRAP_lookup.cruise_transit_notes)
+
+GDF.write(joinpath(OUTPUT_DIR, "rrap_shared_lookup.gpkg"), RRAP_lookup; crs=GFT.EPSG(4326))
